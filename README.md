ed5f82b052e5e76fe59f772b7d7200450c67892b
laoneo <allon.moritz@digital-peak.com>
2014-05-07 11:07:17 +0200

    Initial commit with a simple example

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-plg_dpcalendar_helloworld
+Hello world example for the Joomla calendar DPCalendar
 =========================
 
 Hello world example how to integrate external events into DPCalendar.
+
+PREREQUISITS
+------------
+- Joomla 3.x
+- mysql >= 5.0.0
+- PHP >= 5.3.0
+- DPCalendar 4.x
+
+INSTALLATION
+------------
+Download the whole repository and copy it to the folder plugins/dpcalendar/dpcalendar_helloworld.
+Then go to the extension manager in joomla and discover for new plugins. You should find the hello world plugin.
+
+CONFIGURATION
+------------
+After installation go to the plugin manager and search for hello and edit the hello world plugin. Create a new calendar
+with the name "Demo" and publish it. In the DPCalendar menu link you should be able to select now the "Demo" calendar.
---
+++ dpcalendar_helloworld.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package    DPCalendar
+ * @author     Digital Peak http://www.digital-peak.com
+ * @copyright  Copyright (C) 2007 - 2014 Digital Peak. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl.html GNU/GPL
+ */
+defined('_JEXEC') or die();
+
+JLoader::import('components.com_dpcalendar.libraries.dpcalendar.plugin', JPATH_ADMINISTRATOR);
+if (! class_exists('DPCalendarPlugin'))
+{
+	return;
+}
+
+class PlgDPCalendarDPCalendar_Helloworld extends DPCalendarPlugin
+{
+
+	protected $identifier = 'hw';
+
+	protected function getContent ($calendarId, JDate $startDate = null, JDate $endDate = null, JRegistry $options)
+	{
+		$start = DPCalendarHelper::getDate();
+		$start->modify('+1 day');
+
+		$end = clone $start;
+		$end->modify('+2 hour');
+
+		$buffer = "BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//DPCalendar
+BEGIN:VEVENT
+UID:sdfsf8erhklf89zrjklh890hoih89
+DTSTAMP:20140714T170000Z
+ORGANIZER;CN=John Doe:MAILTO:john.doe@example.com
+DTSTART:" . $start->format('Ymd\THis\Z') . "
+DTEND:" . $end->format('Ymd\THis\Z') . "
+SUMMARY:Demo Hello world event
+DESCRIPTION:This is a demo event in the hello world DPCalendar plugin.
+LOCATION:New York
+END:VEVENT
+END:VCALENDAR";
+		return $buffer;
+	}
+}
